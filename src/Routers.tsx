1320943d506de4b58e733254a6bc3251c6007821
odd:odd <eehd80yul@gmail.com>
2024-03-07 18:27:55 +0900

    first commit

---
+++ src/Routers.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+const Routers = () => {
+    return (
+        <BrowserRouter>
+            <Route></Route>
+        </BrowserRouter>
+    )
+}
+
+export default Routers
